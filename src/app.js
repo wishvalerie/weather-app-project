a9e83ecb83c6ba02d24b8a92e296b6631a8af2e9
wishvalerie <wishvaleriee@gmail.com>
2022-07-24 18:33:45 +0300

    Updated the time

--- src/app.js
+++ src/app.js
@@ -26,6 +26,33 @@ function displayWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+}
+
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Sarurday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
 }
 
 let form = document.querySelector("#search-form");
